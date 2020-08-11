77bbe152bd7f0d53a0f14e6edbb7ae7e8bba15df
Chuck <chuckthemole@gmail.com>
2020-08-11 00:26:50 -0700

    frontend work, displayed locations on index.

--- rock/forms.py
+++ rock/forms.py
@@ -3,6 +3,6 @@ from .models import *
 
 class Sport_Location_Form(forms.ModelForm):
     class Meta:
-        model = Sport_Location
+        model = Location
         fields = ["sport_location_img"]
         #exclude = ('rocker', 'location')
