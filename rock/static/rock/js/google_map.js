de100197102c6be649eb0e5566fb74092e39df1a
Chuck <chuckthemole@gmail.com>
2020-08-13 17:24:06 -0700

    working on icons for maps

--- rock/static/rock/js/google_map.js
+++ rock/static/rock/js/google_map.js
@@ -13,7 +13,20 @@ function load_map(latitude, longitude) {
     var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
+    var main_marker = marker.getIcon();
+
+    marker.addListener('mouseover', function() {
+      //map.setZoom(8);
+      //map.setCenter(marker.getPosition());
+
+      marker.setIcon('https://img.icons8.com/doodle/48/000000/basketball--v1.png');
+    });
+    marker.addListener('mouseout', function() {
+      marker.setIcon(main_marker);
+    });
   };
+
+  //google.maps.event.addDomListener(window, 'load', initialize);
 
   // Append the 'script' element to 'head'
   document.head.appendChild(script);
@@ -28,20 +41,35 @@ function load_map_multiple_markers(coordinates) {
   window.initMap = function() {
     var uluru = {lat: coordinates[0][0], lng: coordinates[0][1]};
     var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: uluru});
-    var marker, i;
-      for (i = 0; i < coordinates.length; i++) {
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
-          map: map
-        });
+    var marker = [];
+    var main_marker = [];
+    var i;
 
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-          return function() {
-            //infowindow.setContent(coordinates[i][2]);
-            infowindow.open(map, marker);
-          }
-        })(marker, i));
-      }
+    for (i = 0; i < coordinates.length; i++) {
+      marker[i] = new google.maps.Marker({
+        position: new google.maps.LatLng(coordinates[i][0], coordinates[i][1]),
+        map: map
+      });
+      main_marker[i] = marker[i].getIcon();
+
+      marker[i].addListener('mouseover', function() {
+        //map.setZoom(8);
+        //map.setCenter(marker.getPosition());
+
+        marker[i].setIcon('https://img.icons8.com/doodle/48/000000/basketball--v1.png');
+      });
+      marker[i].addListener('mouseout', function() {
+        marker[i].setIcon(main_marker[i]);
+      });
+
+//        google.maps.event.addListener(marker[i], 'click', (function(marker[i], i) {
+//        return function() {
+          // To do: if clicked move to that location in window
+          //infowindow.setContent(coordinates[i][2]);
+  //        infowindow.open(map, marker[i]);
+    //    }
+      //})(marker[i], i));
     }
+  }
   document.head.appendChild(script);
 }
